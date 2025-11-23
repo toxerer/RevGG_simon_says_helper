c261df7d280267a2ad1c60c795579ca4eece7a92
Toxer <31672917+toxerer@users.noreply.github.com>
2025-11-23 16:39:16 +0100

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,50 +1,66 @@
 let clickCount = 0;
 const maxClicks = 8; // maksymalna liczba kliknięć
+const clickHistory = []; // historia kliknięć w kolejności
 
 document.querySelectorAll('.circle .item').forEach((item) => {
-  // Każdy kwadrat przechowuje własną tablicę kliknięć
   item.clickNumbers = [];
 
   item.addEventListener('click', () => {
-    // Jeśli przekroczono maksymalną liczbę kliknięć, przerywamy
     if (clickCount >= maxClicks) return;
 
     clickCount++;
     item.clickNumbers.push(clickCount);
+    clickHistory.push(item); // zapisujemy, który element był kliknięty
 
-    // Czyścimy poprzednie wyświetlenia numerów
-    item.innerHTML = '';
-
-    // Tworzymy div z numerami, oddzielone przecinkami
-    const arrow = document.createElement('div');
-    arrow.classList.add('arrow-number');
-
-    var icon = '';
-
-    if (item.classList.contains('item-top')) {
-      icon = '<i class="bi bi-arrow-down-short"></i>';
-      arrow.innerHTML = item.clickNumbers.join(', ') + '<br>' + icon;
-    }
-    else if (item.classList.contains('item-bottom')) {
-      icon = '<i class="bi bi-arrow-up-short"></i>';
-      arrow.innerHTML = icon + '<br>' + item.clickNumbers.join(', ');
-    }
-    else if (item.classList.contains('item-left')) {
-      icon = '<i class="bi bi-arrow-right-short"></i>';
-      arrow.innerHTML = item.clickNumbers.join(', ') + icon;
-    }
-    else {
-      icon = '<i class="bi bi-arrow-left-short"></i>';
-      arrow.innerHTML = icon + item.clickNumbers.join(', ') + '<br>';
-    }
-
-    item.appendChild(arrow);
+    updateItemDisplay(item);
   });
 });
 
-// Funkcja resetująca
+// Funkcja aktualizująca wyświetlanie numerów + ikon
+function updateItemDisplay(item) {
+  item.innerHTML = '';
+
+  const arrow = document.createElement('div');
+  arrow.classList.add('arrow-number');
+
+  let icon = '';
+
+  if (item.classList.contains('item-top')) {
+    icon = '<i class="bi bi-arrow-down-short"></i>';
+    arrow.innerHTML = item.clickNumbers.join(', ') + '<br>' + icon;
+  } else if (item.classList.contains('item-bottom')) {
+    icon = '<i class="bi bi-arrow-up-short"></i>';
+    arrow.innerHTML = icon + '<br>' + item.clickNumbers.join(', ');
+  } else if (item.classList.contains('item-left')) {
+    icon = '<i class="bi bi-arrow-right-short"></i>';
+    arrow.innerHTML = item.clickNumbers.join(', ') + icon;
+  } else {
+    icon = '<i class="bi bi-arrow-left-short"></i>';
+    arrow.innerHTML = icon + item.clickNumbers.join(', ') + '<br>';
+  }
+
+  item.appendChild(arrow);
+}
+
+// Cofnięcie ostatniego kliknięcia
+function undoLastClick() {
+  if (clickHistory.length === 0) return;
+
+  const lastItem = clickHistory.pop();
+  lastItem.clickNumbers.pop(); // usuwamy ostatni numer
+  clickCount--;
+
+  if (lastItem.clickNumbers.length === 0) {
+    lastItem.innerHTML = '';
+  } else {
+    updateItemDisplay(lastItem);
+  }
+}
+
+// Reset wszystkich kliknięć
 function resetClicks() {
   clickCount = 0;
+  clickHistory.length = 0;
   document.querySelectorAll('.circle .item').forEach(item => {
     item.clickNumbers = [];
     item.innerHTML = '';
